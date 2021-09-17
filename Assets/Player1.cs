70fa5a463e4256ad489febc32c8f85d7907577f6
KrisComp16 <90899461+KrisComp16@users.noreply.github.com>
2021-09-17 15:09:26 +0100

    Movement added

---
+++ Assets/Player1.cs
@@ -0,0 +1,76 @@
+
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DoJump();
+        DoMove();
+
+    }
+
+    void DoJump()
+    {
+        Vector2 velocity = rb.velocity;
+
+        // check for jump
+        if (Input.GetKey("w") && (isGrounded==true) )
+        {
+            if (velocity.y < 0.01f)
+            {
+                velocity.y = 6f;    // give the player a velocity of 5 in the y axis
+
+            }
+        }
+
+        rb.velocity = velocity;
+
+    }
+
+    void DoMove()
+    {
+        Vector2 velocity = rb.velocity;
+
+        // stop player sliding when not pressing left or right
+        velocity.x = 0;
+
+        // check for moving left
+        if (Input.GetKey("a"))
+        {
+            velocity.x = -5;
+        }
+
+        // check for moving right
+        if (Input.GetKey("d"))
+        {
+            velocity.x = 5;
+        }
+        rb.velocity = velocity;
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collosion)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isGrounded = false;
+    }
+}
