f0a8decf631a7726c1c111ce5c914488e8ca3e9f
KrisComp16 <90899461+KrisComp16@users.noreply.github.com>
2021-09-24 16:12:52 +0100

    changes

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    public GameObject Player1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float ex = transform.position.x;
+
+        float px = Player1.transform.position.x;
+
+
+
+
+        void DoFaceLeft(bool faceleft)
+        {
+            if (faceleft == true)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+
+
+        if (ex > px)
+        {
+            DoFaceLeft(true);
+        }
+        else
+        {
+            DoFaceLeft(false);
+        }
+
+
+
+    }
+
+}
